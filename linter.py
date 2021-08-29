932fdb2126502c4833332b8fb7871826c19198e2
Nando Vieira <me@fnando.com>
2021-08-29 10:03:16 -0700

    Make it work with latest SublimeLinter api.
    https://github.com/amperser/SublimeLinter-contrib-proselint/pull/10

--- linter.py
+++ linter.py
@@ -6,33 +6,20 @@
 # Copyright (c) 2015 Jordan Suchow
 #
 #
-
 """This module exports the Proselint plugin class."""
 
 from SublimeLinter.lint import Linter
 
 
 class Proselint(Linter):
+  """Provides an interface to proselint."""
 
-    """Provides an interface to proselint."""
-
-    syntax = ('*')
-    cmd = 'proselint'
-    executable = None
-    version_args = '--version'
-    version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 0.0.0'
-    regex = (
-        r'^.+?:(?P<line>\d+):(?P<col>\d+): \S* (?P<message>.+)'
-    )
-    multiline = True
-    line_col_base = (1, 1)
-    tempfile_suffix = 'pltmp'
-    selectors = {
-        '*': 'text.html.markdown, text.plain, text.tex.latex, comment'
-    }
-    word_re = None
-    defaults = {}
-    inline_settings = None
-    inline_overrides = None
-    comment_re = None
+  cmd = 'proselint'
+  regex = (r'^.+?:(?P<line>\d+):(?P<col>\d+): \S* (?P<message>.+)')
+  multiline = True
+  line_col_base = (1, 1)
+  tempfile_suffix = 'pltmp'
+  word_re = None
+  defaults = {
+      "selector": 'text.html.markdown, text.plain, text.tex.latex, comment'
+  }
